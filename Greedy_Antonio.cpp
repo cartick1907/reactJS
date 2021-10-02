2c33d1b372e317fad426dfbad2ab4ca8ee9ced9b
cartick1907 <kartikbhatt1299@gmail.com>
2021-10-02 12:07:40 +0530

    hello

---
+++ Greedy_Antonio.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+int main()
+{
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, m;
+    cin >> n >> m;
+    while (m--)
+    {
+        int x, y, c;
+        cin >> x >> y >> c;
+    }
+
+    return 0;
+}
